fde370ebcfdd8e6c6912321b23051af3ffb405b9
juanluisrosaramos <monete@gmail.com>
2019-04-30 15:52:55 +0200

    resnet

--- research/slim/scripts/finetune_resnet_v1_50_on_flowers.sh
+++ research/slim/scripts/finetune_resnet_v1_50_on_flowers.sh
@@ -47,7 +47,7 @@ fi
 # Fine-tune only the new layers for 3000 steps.
 python train_image_classifier.py \
   --train_dir=${TRAIN_DIR} \
-  --dataset_name=flowers \
+  --dataset_name=smiletronix \
   --dataset_split_name=train \
   --dataset_dir=${DATASET_DIR} \
   --model_name=resnet_v1_50 \
